eea7a9294e134dd615ad00a80fa0ed6bce29feda
Lululeslie <leslieganoubong@gmail.com>
2023-11-01 20:53:11 +0100

    mise en place du terminal

--- main.py
+++ main.py
@@ -9,9 +9,24 @@ Projet: 19
 import networkx as nx
 import matplotlib.pyplot as plt
 from simulation import run_simulation
+from agent import Agent
 
 def main():
     run_simulation()
 
 if __name__ == "__main__":
-    main()
+
+# Demande à l'utilisateur combien d'agents et de communautés il souhaite créer
+agent = Agent()
+communities = create_communities()
+agent = int(input("Combien d'agents (sommets) voulez-vous créer ? "))
+communities = int(input("Combien de communautés souhaitez-vous créer ? "))
+
+# Vérifie si les valeurs entrées sont dans les limites acceptables
+if agent > 700 or communities > 10:
+    print("C'est trop grand ! Veuillez entrer une valeur plus petite.")
+else:
+    print(f"Vous avez choisi {agent} agent. et {communities} communautés.")
+    # Vous pouvez ajouter le code pour travailler avec ces valeurs ici.
+main()
+   
